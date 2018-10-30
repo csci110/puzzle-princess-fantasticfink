6a9f65b95b9fde30d863854fb774836a6bf9e338
Rob Fink <finkr@dewv.edu>
2018-10-30 01:08:15 +0000

    Updates via sync script

--- puzzledPrincess.js
+++ puzzledPrincess.js
@@ -1,10 +1,28 @@
-import {game, Sprite} from "./sgc/sgc.js";
+import { game, Sprite } from "./sgc/sgc.js";
 game.setBackground("floor.png");
 
-class Marker extends Sprite {   }
-class PrincessMarker extends Marker {   }
-class StrangerMarker extends Marker {   }
-class TicTacToe  extends Sprite {   }
+class Marker extends Sprite {}
+
+class PrincessMarker extends Marker {}
+
+class StrangerMarker extends Marker {}
+
+class TicTacToe extends Sprite {
+    constructor() {
+        super();
+        this.name = "board";
+        this.setImage("board.png");
+        this.x = 300;
+        this.y = 85;
+        this.SquareSize = 150;
+        this.size = 3;
+        this.activeMarker; // variable exists, but value is undefined
+    }
+
+    takeTurns(){
+        this.activeMarker = new PrincessMarker(this);
+    }
+}
 
 let theBoard = new TicTacToe();
 theBoard.takeTurns();
